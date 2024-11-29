e998e5a26c806fe348603d873d1ca54a89378543
Teoman Köylüoglu <teoman@kth.se>
2024-11-29 17:57:50 +0100

    pudh

---
+++ src/apiCall.js
@@ -0,0 +1,6 @@
+import {URL} from "/src/apiConfig.js";
+
+export function testAPI() {
+    console.log("This is the test API call");
+    
+}
---
+++ src/apiConfig.js
@@ -0,0 +1,1 @@
+export const URL =  "https://api.artic.edu/api/v1/";
