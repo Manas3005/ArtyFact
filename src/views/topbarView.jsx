df6043dedcafff84e438bcac61488b0fe3ed5a2e
Manas Ahuja <manasah@kth.se>
2024-11-30 11:56:16 +0100

    Fix Explore, Logo, add search bar
    Co-Authored-By: Dawa Arkhang <23603+arkhang@users.noreply.gits-15.sys.kth.se>

--- src/views/topbarView.jsx
+++ src/views/topbarView.jsx
@@ -1,15 +1,29 @@
 import "/src/style.css"
+//import "/buttons/explore.png"
 
 export function TopBarView(props){
 
     return (
         <div>
-            <div>
-                <button> Explore </button>
-                <span className="topBar">ArtyFact</span>
-            </div>
-        </div>
+            <div className="topBar">
+                
+                <button  className="Explore"> 
+                   <img  src="image/explore.png"/>
+                </button>
 
+               <input className="searchBar" placeholder = "Search..."/> 
+
+
+                    <div> 
+                    <img  src = "image/Logo.png" />
+                     </div>
+
+                
+
+            </div>
+            
+
+        </div>
 
 
     ) 
