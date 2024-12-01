ea1f29938f847baa939d57d6f1c9bde965dd5d87
Raghav Khurana <rkhurana@kth.se>
2024-12-01 17:55:03 +0100

    Add Text Panel for Art Descriptions

--- src/views/homePageViews/artDescBodyView.jsx
+++ src/views/homePageViews/artDescBodyView.jsx
@@ -3,10 +3,17 @@ import "/src/style.css"
 function ArtDescBodyView (props) {
     return (
 
-        <div>
-            <img className="ArtDescPanel"
-            src= "image/artDescImage.png" 
-            />
+        <div className="horizontalFlexParent"> 
+
+            <img className="ArtDescImage" src= "image/artdeschomepage.png" />
+            <div className="ArtDescTextPanel"> 
+
+                <span className="ArtDescName">Land of Bradbury</span>
+                <p className="ArtDescText">The painting captures a stark and evocative scene in black and white, amplifying its sense of desolation and quiet beauty. 
+                    A long, narrow road stretches into the distance, leading the viewer's eye toward a barren expanse of land. 
+                    The terrain appears cracked and dry, emphasizing the harshness of the environment. 
+                    On the horizon, a cluster of buildings sits in solitude, their angular forms silhouetted against the pale, featureless sky.</p>
+            </div>
         </div>
     )
 }
