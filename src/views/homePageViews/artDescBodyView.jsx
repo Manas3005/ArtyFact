d06b1f0e356fc4c92f940dee1f83450077d3760e
Raghav Khurana <rkhurana@kth.se>
2024-12-03 21:51:31 +0100

    Limit text length of artwork description in ArtDesc to avoid text overflow in the text panel

--- src/views/homePageViews/artDescBodyView.jsx
+++ src/views/homePageViews/artDescBodyView.jsx
@@ -4,6 +4,17 @@ function ArtDescBodyView (props) {
     console.log("-----THIS IS PROPS", props)
     console.log(props.image)
 
+    function limitTextLength (inputtedText){
+        const maxLength = 635;
+
+        if (inputtedText.length < maxLength){
+            return inputtedText
+        }
+    
+        const smallerText = (inputtedText.substring(0, maxLength)) + "..."
+        return smallerText
+    }
+
     return (
 
         <div className="horizontalFlexParent"> 
@@ -12,7 +23,7 @@ function ArtDescBodyView (props) {
             <div className="ArtDescTextPanel"> 
 
                 <p className="ArtDescName">{props.artData.title}</p>
-                <p className="ArtDescText">{props.description}</p>
+                <p className="ArtDescText">{limitTextLength(props.description) }</p>
                 <p className="ArtContext" >  {props.artData.date_display}    </p>
                 <p className="ArtContext"> {props.artData.artist_display}  </p>
                 <p className="ArtContext"> {props.artData.classification_title}</p>
