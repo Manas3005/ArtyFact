8d0bf2ada7bba363369ba05ed186254a44c33fc7
Raghav Khurana <rkhurana@kth.se>
2024-12-04 23:48:04 +0100

    Add prelimary view + presenter for showing journal entry list

--- src/reactjs/myJournalsMainPresenter.jsx
+++ src/reactjs/myJournalsMainPresenter.jsx
@@ -1,11 +1,12 @@
 import { JournalTopBarView } from "../views/myJournalViews/journalTopBarView";
+import { JournalEntriesListView } from "../views/myJournalViews/journalEntriesListView";
 
 function MyJournalsMain (props){
     console.log("Rendering myJournalsMain")
     return (<div>
 
         <JournalTopBarView></JournalTopBarView>
-
+        <JournalEntriesListView></JournalEntriesListView>
     </div>)
 
     
---
+++ src/views/myJournalViews/journalEntriesListView.jsx
@@ -0,0 +1,17 @@
+import "/src/style.css"
+
+export function JournalEntriesListView(props){
+    
+    return (
+        <div className="entriesList">
+            <img src="image/thinkingMan.png"></img>
+            <p className="alertText">No Journal Entries Added Yet. </p>
+            <p className="alertText">Add Journal Entries using the button above. </p>                       
+        </div>
+
+    ) 
+
+}
+    
+
+
