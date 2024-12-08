e380864078f6588b2373d2279d6bf32aae3eae55
Raghav Khurana <rkhurana@kth.se>
2024-12-08 23:59:27 +0100

    Add function for dynmaically adding journalEntries to scrollable list

--- src/views/myJournalViews/journalEntriesListView.jsx
+++ src/views/myJournalViews/journalEntriesListView.jsx
@@ -1,9 +1,11 @@
 import "/src/style.css"
 import "/src/journalsStyle.css"
+import { transformJournalTitleCB } from "../../utilities"; 
 
 
 
 export function JournalEntriesListView(props){
+
     
     if (props.entries.length === 0){
         
@@ -30,12 +32,32 @@ export function JournalEntriesListView(props){
 
                 <div id="scrollableArea" className="scrollable">
 
-                
+                {addDynamicJournalEntries(props.entries)}
+
                 </div>
 
             </div>
         )
 
+        function addDynamicJournalEntries (journalEntries){ 
+            const entryTitles = journalEntries.map(transformJournalTitleCB)
+    
+            entryTitles.forEach(entryTitle => {
+    
+                const scrollableArea = document.getElementById("scrollableArea");
+                const wrapper = document.createElement("div"); // Create a container
+              
+                const img = document.createElement("img");
+                img.src = "image/starry-night.png"                 
+    
+                const text = document.createElement("span"); // Create text element
+                text.textContent = entryTitle;        // Set the text content
+    
+                wrapper.appendChild(img);            // Add image to the wrapper
+                wrapper.appendChild(text);           // Add text to the wrapper
+                scrollableArea.appendChild(wrapper); // Add wrapper to the container
+            });
+        }
 
     } 
 
