54b8da0f62489d84a7336a910057d00eeeb51f12
Teoman Köylüoglu <teoman@kth.se>
2024-12-09 16:19:32 +0100

    Resolved a minor conflict

--- src/reactjs/homepagePresenter.jsx
+++ src/reactjs/homepagePresenter.jsx
@@ -3,7 +3,6 @@ import { useEffect, useState } from "react";
 import { ArtDescBodyView } from "../views/homePageViews/artDescBodyView";
 
 import { testAPI, getArtWorks, getArtWorkImage, URLParamsForImage, getArtWorksSearch, getArtWorksWithLog} from '/src/apiCall.js';
-import { testAPI, getArtWorks, getArtWorkImage, URLParamsForImage, getArtWorksSearch} from '/src/apiCall.js';
 import {cleanHtmlContent } from '/src/utilities.js'
 import { TopBarView } from "../views/homePageViews/topbarView";
 import { ExploreBodyView } from "/src/views/homePageViews/exploreBodyView.jsx";
