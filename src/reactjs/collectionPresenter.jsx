1b0de3de3584ce9c1de87e00e89ff7cd17c407a9
Teoman Köylüoglu <teoman@kth.se>
2024-12-15 23:37:01 +0100

    CollectionListView gets specific collection as prop
    The issue now is that each time we reload the page again then the singleCollectionArray is emptied. This is fine, as this functionality is only used whenever we enter the collection page from the myCollection page. This saves us one API call, but we need to fix the problem of reloading the page and keeping the data persistent

--- src/reactjs/collectionPresenter.jsx
+++ src/reactjs/collectionPresenter.jsx
@@ -1,16 +1,29 @@
 import { TopbarCollectionView } from "../views/collectionViews/topbarCollectionView";
 import { CollectionListview } from "../views/collectionViews/collectionListView";
-
+import { useSelector } from "react-redux";
 
 export function CollectionPresenter() {
 
+    const selectedCollection = useSelector((state) => state.myCollections.singleCollectionArray);
+    console.log("This is the selected collection", selectedCollection);
+
+    if(!selectedCollection ||selectedCollection.length === 0) {
+        return (
+            <div>
+                <TopbarCollectionView></TopbarCollectionView>
+                <p>No collection found!</p>
+            </div>
+        )
+    }
 
     return (
 
         <div>
             <TopbarCollectionView>
             </TopbarCollectionView>
-            <CollectionListview></CollectionListview>
+            <CollectionListview
+            collection={selectedCollection}
+            ></CollectionListview>
         </div>
 
 
--- src/views/myCollectionViews/listOfCollectionsView.jsx
+++ src/views/myCollectionViews/listOfCollectionsView.jsx
@@ -5,20 +5,45 @@ export function ListOfCollectionsView(props) {
 
     console.log("This is the props", props);
 
-    function handleNavigateACB() {
+    function handleNavigateACB(evt, collection) {
+        console.log("This is the evt", evt);
+        console.log("This is the collection in event", collection);
+        props.setCollection(collection);
+        //Det vi behöver är att veta just vilken det är, och för att veta det behöver vi veta vilken collection vi har tryckt på.
+        /**
+         * Hur kan vi veta vilken collection vi har tryckt på?
+         * Vi kan göra det genom att
+         * 1) Associera varje collection med ett id?
+         * 2) Skicka hela objektet när vi trycker på den.
+         * Vad förväntar vi oss att få in i evt? Det är frågan.
+         * Vi har nu faktiskt fått in själva collection. Det vi kan göra nu är att fylla den i application state.
+         * Och då låter vi den collectionPresenter useSelecta den och skicka den som prop ner till collectoinView.
+         */
+
+        /*const newArray = [...props.collections].filter((collection) => {
+            return collection.title == evt.target.value;
+        });*/
+
+        [...props.collections].map((collection) => console.log(collection));
+        //props.setCollection(newArray);
+        /**
+         * Här behöver vi lägga till logik för att skicka data till application state.
+         * vi behöver definiera en ny reducer som tar in ett objekt, där objektet är den specifika collection:en.
+         * 
+         */
         window.location.hash='/thecollection';
     }
 
 
 
-    function renderSlideShowCB(images) {
+    function renderSlideShowCB(images, collection) {
         return [...images].map((image, i) => (
             <img
                 key={i}
                 src={image}
                 alt={`Slide ${i}`}
                 className={`collection-image ${i === props.activeIndex ? "active" : "hidden"}`}
-                onClick={handleNavigateACB}
+                onClick={(evt) => handleNavigateACB(evt, collection)}
                 
             />
         ));
@@ -28,7 +53,7 @@ export function ListOfCollectionsView(props) {
         return (
             <div className="collection-item" key={collection.title}>
                 <div className="image-title">{collection.title}</div>
-                <div className="slideshow-container">{renderSlideShowCB(collection.images)}</div>
+                <div className="slideshow-container">{renderSlideShowCB(collection.images, collection)}</div>
             </div>
         );
     }
