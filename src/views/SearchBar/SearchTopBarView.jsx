d5b0c0a2c4bccf98da4f41752765f7a8ffc534e8
41w1 <arkhang@kth.se>
2024-12-15 15:40:06 +0100

    Home button is functional from search

--- src/views/SearchBar/SearchTopBarView.jsx
+++ src/views/SearchBar/SearchTopBarView.jsx
@@ -4,23 +4,35 @@
 import { testAPI, getArtWorks, getArtWorkImage, URLParamsForImage, getArtWorksSearch} from '/src/apiCall.js';
 
 export function SearchTopBar(props){
-
-
-
-console.log("THIS IS THE PROPS THAT IS PASSED TO SEARCHVIEW",props)
-    
+console.log("THIS IS THE PROPS THAT IS PASSED TO SEARCHVIEW",props)   
 let hello = props.artworks.data[0].image_id
+let allData = props.artworks.data
 let image = URLParamsForImage(hello)
+
+
+function eventHandlerForHomeClickACB(evt) {
+    window.location.hash="#/homepage";        
+}
+
 
 return (
   <div>
-
-    <button>  Home  </button>
-    <img src= {image} />
-    
-
+    <button onClick={eventHandlerForHomeClickACB}>  Home  </button>
+     {/*[...props.allData].map(renderSearchResultsCB)*/} 
 
   </div>  
 )
+
+
+function renderSearchResultsCB(result) {  
+        
+    return (
+    <span key={result.id} >     
+      <img src={URLParamsForImage(result.image_id)} height="100"></img>
+    
+    </span>
+);
+
+}
 
 }
